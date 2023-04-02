92d8813c5168d8e81e35f8bfb26d13a19c808399
Backend Man <backend69@proton.me>
2023-04-02 03:02:40 +0200

    first commit

---
+++ scripts/start-docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+KEY="dev0"
+CHAINID="qom_7668378-1"
+MONIKER="mymoniker"
+DATA_DIR=$(mktemp -d -t qom-datadir.XXXXX)
+
+echo "create and add new keys"
+./qomd keys add $KEY --home $DATA_DIR --no-backup --chain-id $CHAINID --algo "eth_secp256k1" --keyring-backend test
+echo "init Evmos with moniker=$MONIKER and chain-id=$CHAINID"
+./qomd init $MONIKER --chain-id $CHAINID --home $DATA_DIR
+echo "prepare genesis: Allocate genesis accounts"
+./qomd add-genesis-account \
+"$(./qomd keys show $KEY -a --home $DATA_DIR --keyring-backend test)" 1000000000000000000aqom,1000000000000000000stake \
+--home $DATA_DIR --keyring-backend test
+echo "prepare genesis: Sign genesis transaction"
+./qomd gentx $KEY 1000000000000000000stake --keyring-backend test --home $DATA_DIR --keyring-backend test --chain-id $CHAINID
+echo "prepare genesis: Collect genesis tx"
+./qomd collect-gentxs --home $DATA_DIR
+echo "prepare genesis: Run validate-genesis to ensure everything worked and that the genesis file is setup correctly"
+./qomd validate-genesis --home $DATA_DIR
+
+echo "starting qom node $i in background ..."
+./qomd start --pruning=nothing --rpc.unsafe \
+--keyring-backend test --home $DATA_DIR \
+>$DATA_DIR/node.log 2>&1 & disown
+
+echo "started qom node"
+tail -f /dev/null
